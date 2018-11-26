48704e04d21b1b73fc208147345620ee21c854b8
jiang-maker <137764155@qq.com>
2018-11-26 18:08:53 +0800

    video test

--- src/static/pc/js/myVideo.js
+++ src/static/pc/js/myVideo.js
@@ -1,8 +1,34 @@
 (function($){
-  $.fn.MyVideo = function () {
-    
+  $.fn.MyVideo = function (options) {
+    var opts = $.extend({}, defaults, options);
+    init(opts);
+    // console.log($(this));
   }
-  $.fn.MyVideo.defaults = {
-      
+  $.fn.MyVideo.prototype = {
+    play: function(obj){
+      console.log(obj);
+    },
+    pause: function(obj){
+
+    },
+    resize: function(obj){
+
+    }
   }
+  function init(obj){
+    console.log(obj)
+    $(obj.id).append("<video width='"+ obj.width+"'"+"height="+obj.height+">"+
+                      "<source src='"+ obj.src +"' type='video/mp4'>"+
+                    +"</video>");
+  }
+  var defaults = {
+    id: undefined,          //视频容器  id
+    width: undefined,       //宽
+    height: undefined,      //高
+    poster: '',      //视频封面
+    src: undefined,         //视频源地址
+    controls: false,         //视频控件
+    fullscreen: false       //全屏
+  }
+  // window.MyVideo = MyVideo;
 })(jQuery)
