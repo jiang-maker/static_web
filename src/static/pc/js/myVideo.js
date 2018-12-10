3a3eb151ceccdd6b93031e70742b8741b1526b50
jiang-maker <137764155@qq.com>
2018-12-10 17:50:23 +0800

    test

--- src/static/pc/js/myVideo.js
+++ src/static/pc/js/myVideo.js
@@ -38,4 +38,102 @@
     fullscreen: false       //全屏
   }
   // window.MyVideo = MyVideo;
+  
+  $('.play').click(function() {
+    $(this)
+      .find('button')
+      .css({
+        'background-position': '-25px 0'
+      })
+  })
+  $('.vol button').click(function() {
+    $(this).css({
+      'background-position': '-72px 0'
+    })
+  })
+
+  var tag = false,
+    ox = 0,
+    left = 0,
+    bgleft = 0,
+    percent = 0
+  $('.progress-btn').mousedown(function(e) {
+    ox = e.pageX - left
+    tag = true
+  })
+  $(document).mouseup(function() {
+    tag = false
+  })
+  $('.progress').mousemove(function(e) {
+    //鼠标移动
+    if (tag) {
+      left = e.pageX - ox
+      if (left <= 0) {
+        left = 0
+      } else if (left > 600) {
+        left = 600
+      }
+      $('.progress-btn').css('left', left)
+      $('.progress-current').width(left)
+      percent = parseInt((left / 600) * 100)
+    }
+  })
+  $('.progress-total').click(function(e) {
+    //鼠标点击
+    if (!tag) {
+      bgleft = $('.progress-total').offset().left
+      left = e.pageX - bgleft
+      if (left <= 0) {
+        left = 0
+      } else if (left > 600) {
+        left = 600
+      }
+      $('.progress-btn').animate({ left: left }, 300)
+      $('.progress-current').animate({ width: left }, 300)
+      percent = parseInt((left / 600) * 100)
+    }
+  })
+
+  var vtag = false,
+    oy = 0,
+    height = 0,
+    bgheight = 0,
+    vpercent = 0
+  $('.vol-progress-btn').mousedown(function(e) {
+    oy = e.pageY
+    vtag = true
+  })
+  $(document).mouseup(function() {
+    vtag = false
+  })
+  $('.vol-progress').mousemove(function(e) {
+    //鼠标移动
+    if (vtag) {
+      height = oy + 72 - e.pageY
+      if (height <= 0) {
+        height = 0
+      } else if (height > 72) {
+        height = 72
+      }
+      $('.vol-progress-btn').css('bottom', height)
+      $('.vol-progress-current').height(height)
+      vpercent = parseInt((height / 72) * 100)
+    }
+  })
+  $('.vol-progress-total').click(function(e) {
+    //鼠标点击
+    if (!vtag) {
+      bgheight = $('.vol-progress-total').offset().top
+      height = bgheight + 72 - e.pageY
+      // console.log(height);
+      if (height <= 0) {
+        height = 0
+      } else if (height > 72) {
+        height = 72
+      }
+      $('.vol-progress-btn').animate({ bottom: height }, 300)
+      $('.vol-progress-current').animate({ height: height }, 300)
+      vpercent = parseInt((height / 600) * 100)
+    }
+  })
 })(jQuery)
