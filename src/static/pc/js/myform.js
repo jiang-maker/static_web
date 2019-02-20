ae21e1a24433e2366ed426dd4574ffa0101d551d
jiang-maker <137764155@qq.com>
2019-02-20 16:24:23 +0800

    new

---
+++ src/static/pc/js/myform.js
@@ -0,0 +1,18 @@
+$.validator.setDefaults({
+  submitHandler: function() {
+    alert("提交事件!");
+  }
+});
+$().ready(function() {
+  // 在键盘按下并释放及提交后验证提交表单
+  $("#signupForm").validate({
+      rules: {
+        firstname: "required",
+        lastname: "required"
+      },
+      messages: {
+        firstname: "请输入您的名字",
+        lastname: "请输入您的姓氏"
+      }
+  });
+});
